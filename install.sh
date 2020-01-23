a40f402eec4ee4c41ca840f69b6ed4ccddf6c0f2
kemalcanbora <kemalcanbora@gmail.com>
2020-01-23 14:59:26 +0300

    install sh

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 !/bin/sh
 alias python=python3
 apt -y update
+apt-get -y install git
 apt-get -y install build-essential
-apt-get -y apt-get install docker
-apt-get -y apt-get install docker-compose
+apt-get -y install docker
+apt-get -y install docker.io
+apt-get -y install docker-compose
 apt-get -y install supervisor
-apt -y install python3-pip
+apt-get -y install python3-pip
 git clone https://github.com/kemalcanbora/ba_bigdata_docker
 cd ba_bigdata_docker
 docker-compose up -d
 cd
-mv /home/ba_bigdata_docker/*.conf /etc/supervisor/conf.d
+mv /home/ba/ba_bigdata_docker/*.conf /etc/supervisor/conf.d
 supervisorctl reread
 supervisorctl update
 supervisorctl reread
